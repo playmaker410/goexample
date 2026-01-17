8726589886e01e3ab1da9ee8f132ef544e668a91
playmaker410 <odinakaoshim@gmail.com>
2026-01-17 18:30:52 +0000

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/playmaker410/myproject
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+import ("fmt")
+
+func main(){
+
+var name1 string = "Ada"
+var name2 string = "Chiemerie"
+var age1 int =10
+var age2 int = 20
+var price,size,age,health int = 150, 20 ,40 , 50
+//VARIABLW DECLERATION
+fmt.Println(name1)
+fmt.Println(name2)
+fmt.Println(age1)
+fmt.Println(age2)
+fmt.Println(price)
+fmt.Println(size)
+fmt.Println(age)
+fmt.Println(health)
+
+}
+
+
+func main1(){
+const PI  = 3.43
+fmt.Println(PI)
+
+}
